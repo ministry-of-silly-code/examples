0c275efa68e3181ecd27686f63384114220b0f06
d3sm0 <simone.totaro@gmail.com>
2021-08-18 03:23:04 +0200

    adress pr comments

--- config.py
+++ config.py
@@ -25,9 +25,4 @@ experiment_buddy.register(locals())
 learning_rate = jax.experimental.optimizers.inverse_time_decay(initial_lr, decay_steps, decay_factor, staircase=True)
 eval_every = math.ceil(num_epochs / 1000)
 
-sweep_yaml = ""
-
-if os.environ.get('SWEEP', ""):
-    sweep_yaml = "sweep.yaml"
-
-tensorboard = experiment_buddy.deploy(host=os.environ.get('BUDDY_HOST', ""), sweep_yaml=sweep_yaml)
+tensorboard = experiment_buddy.deploy(host=os.environ.get('BUDDY_HOST', ""), sweep_yaml=os.environ.get('SWEEP', ""))
